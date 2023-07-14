3a7cebe7f102c0caf009a1490baafdc24f34ee70
topikk <118111616+taufiqpr@users.noreply.github.com>
2023-07-14 19:27:24 +0700

    model.java

---
+++ model.java
@@ -0,0 +1,75 @@
+package Model;
+
+import java.util.List;
+
+public class Product {
+    private int id;
+    private String title;
+    private String description;
+    private double price;
+    private double discountPercentage;
+    private double rating;
+    private int stock;
+    private String brand;
+    private String category;
+    private String thumbnail;
+    private List<String> images;
+
+    public Product(int id, String title, String description, double price, double discountPercentage, double rating, int stock, String brand, String category, String thumbnail, List<String> images) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.discountPercentage = discountPercentage;
+        this.rating = rating;
+        this.stock = stock;
+        this.brand = brand;
+        this.category = category;
+        this.thumbnail = thumbnail;
+        this.images = images;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public double getDiscountPercentage() {
+        return discountPercentage;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public List<String> getImages() {
+        return images;
+    }
+}
